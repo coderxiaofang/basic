e30c31a76c91fb4ea753ba828c36dec640e91330
coderxiaofang <166970522+coderxiaofang@users.noreply.github.com>
2024-04-21 15:16:30 +0800

    Update method.java

--- IdeaProjects/objectProject/src/com/myblog/javaproject/method.java
+++ IdeaProjects/objectProject/src/com/myblog/javaproject/method.java
@@ -1,23 +1,15 @@
-package com.myblog.javaproject;
 
-import java.util.Scanner;
-
-public class method {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int i = sc.nextInt();
-        double v = sc.nextDouble();
-        //ctrl+alt+v自动补齐
-        //ctrl+alt+左移动键，快速跳到光标的上一个位置
         /*
-         ctrl+alt+L格式化代码，然代码更规范
-         ctrl+D向下复制
-         ctrl+当忘记调用方法的参数显示各参数
-         ctrl+alt+M提取方法，可定义方法名
-         home快速定位到行首，end快速定位到行尾
-         Alt加鼠标左键竖着选中多个
-         快速生成JavaBean方法：Alt+insert或者右键点击Ptg to Javabean
-         */
-        //ctrl+f 全文查找
-    }
-}
+        ctrl+alt+左移动键，快速跳到光标的上一个位置
+        ctrl+alt+v自动补齐
+        ctrl+alt+L格式化代码，然代码更规范
+        ctrl+D向下复制
+        ctrl+当忘记调用方法的参数显示各参数
+        ctrl+alt+M提取方法，可定义方法名
+        home快速定位到行首，end快速定位到行尾
+        Alt加鼠标左键竖着选中多个
+        Alt+insert或者右键点击Ptg to Javabean，快速生成JavaBean方法：
+        ctrl+f 全文查找
+        ctrl+alt+T 添加while循环
+        */
+  
