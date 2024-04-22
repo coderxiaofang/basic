49cf701aa227ed3e77c812b5fbdf533efa9fde3f
coderxiaofang <166970522+coderxiaofang@users.noreply.github.com>
2024-04-22 08:59:01 +0800

    Update objectExamp4.22.java

--- IdeaProjects/objectProject/src/com/myblog/javaproject/objectExamp4.19.java
+++ IdeaProjects/objectProject/src/com/myblog/javaproject/objectExamp4.19.java
@@ -161,8 +161,163 @@
 //    }
 //}
 //
+           //package com.myblog.javaproject;
+//
+//import java.util.Random;
+//
+//public class test3 {
+//    //五位验证码，四个字母一个数字，任意位置
+//    public static void main(String[] args) {
+//        Random r=new Random();
+//        String[] resultArr=new String[5];
+//        StringBuilder sbr=new StringBuilder();
+//        for (int i = 0; i < 4; i++) {
+//            sbr.append(GenerateJame(r));
+//        }
+//        int randomNum=GenerateNum(r,10,0);
+//        int randomString=GenerateNum(r,5,0);
+//        //方法一，利用insert插入，方法二，随机截取插入
+//        String result=sbr.insert(randomString,randomNum).toString();
+////        String result=sbr.substring(0,randomString)+(randomNum)+sbr.substring(randomString);
+//        System.out.println(result);
+//    }
+//    //随机生成一个大小写字母
+//    public static char GenerateJame(Random r){
+//        //65~65+25:大写,//97~97+25：小写
+//        char jame;
+//        boolean PossibleNum=r.nextBoolean();
+//        if(PossibleNum){
+//             jame=(char)(GenerateNum(r,25,97));
+//        }else {
+//             jame=(char)(GenerateNum(r,25,65));
+//        }
+//        return jame;
+//    }
+//    //生成一个随机数
+//    public static int GenerateNum(Random r,int a,int b){
+//        int RandomNum=r.nextInt(a)+b;
+//        return RandomNum;
+//    }
+//}
+//
 
 
+
+
+            //package com.myblog.javaproject;
+//
+//import java.util.Random;
+//
+//public class test3 {
+//    public static void main(String[] args) {
+//        //由若干单词组成的字符串，计算其最后一个单词的长度
+//        String str="how are your";
+//        for (int i = str.length()-1; i > 0; i--) {
+//            char chs=str.charAt(i);
+//            if(chs==' '){
+//                System.out.println(str.substring(i+1).length());
+//                break;
+//            }
+//        }
+//    }
+//}
+//
+
+
+
+
+            //package com.myblog.javaproject;
+//
+//import java.util.Scanner;
+//
+//public class TestStudent {
+//    //存储学生信息，包含判断是否能添加，是否能删除等
+//    public static void main(String[] args) {
+//        Student g1 = new Student(1, "小明", 11);
+//        Student g2 = new Student();
+//        Student[] arrAge = new Student[3];
+//        arrAge[0] = g1;
+//        //添加一个学生，进行学号唯一判断
+//        Scanner sc = new Scanner(System.in);
+//        arrAge[1] = g2;
+//        System.out.println("请输入你要添加的学生的学号：");
+//        int age = sc.nextInt();
+//        //信息不存在则增添
+//        append(arrAge, age,1,sc);
+//        System.out.println(g2.getStuNumber() + "," + g2.getAge() + "," + g2.getName());
+//        System.out.println("请输入你要删除的学生信息：");
+//        int deleteStu=sc.nextInt();
+//        //判断学生信息是否存在，存在即可删除
+//        delete(arrAge,deleteStu,sc);
+//        //判断这两个学号的学生是否存在
+//        exist(arrAge,2,1);
+//        exist(arrAge,4,1);
+//        //打印剩余学生的信息
+//        remain(arrAge);
+//    }
+//    //添加学生信息
+//    public static void append(Student[] arr, int age, int ge, Scanner sc) {
+//        while (true){
+//            boolean flag=true;
+//            if(exist(arr,age,0)) {
+//                arr[ge].setStuNumber(age);
+//                arr[ge].setAge(12);
+//                arr[ge].setName("fang");
+//                break;
+//            }
+//            if(flag) {
+//                System.out.println("学号存在!请重新输入你要添加的学号：");
+//                age = sc.nextInt();
+//            }
+//        }
+//
+//    }
+//    //删除学生信息
+//    public static void delete(Student[] arr, int deleteStu, Scanner sc){
+//            boolean flag=true;
+//            for (int i = 0; i < arr.length; i++) {
+//                if (arr[i]!=null&&deleteStu==arr[i].getStuNumber()){
+//                    //删除该信息，设置为Null即可
+//                    arr[i]=null;
+//                    System.out.println(deleteStu+"要删除的该信息存在，删除成功！");
+//                    flag=false;
+//                    break;
+//                }
+//            }
+//            if(flag){
+//                System.out.println("要删除的该信息不存在，删除失败！");
+//            }
+//    }
+//    //判断该学生的学号是否存在,存在就将年龄加一
+//    public static boolean exist(Student[] arr, int num, int zero){
+//            boolean flag = true;
+//            for (int i = 0; i < arr.length; i++) {
+//                if (arr[i] != null && num == arr[i].getStuNumber()) {
+//                    System.out.println(num+"这个学号存在!固将其年龄加1");
+//                    if (zero==1){
+//                        arr[i].setAge(arr[i].getAge()+1);
+//                    }
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if(flag){
+//                System.out.println(num+"这个学号不存在！");
+//            }
+//            return flag;
+//    }
+////    打印剩余学生的信息
+//    public static void remain(Student[] arrAge){
+//        for (int i = 0; i < arrAge.length; i++) {
+//            if(arrAge[i]!=null){
+//                System.out.println("剩余学生信息为："+arrAge[i].getStuNumber()+"年龄为："+arrAge[i].getAge()+"姓名为："+arrAge[i].getName());
+//            }
+//        }
+//    }
+//}
+//
+//
+//
         }
     }
 }
