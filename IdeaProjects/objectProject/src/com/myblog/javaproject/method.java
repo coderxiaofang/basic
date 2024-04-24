23438e19ad4a35d639a0982472631cdd5c38dd1d
coderxiaofang <166970522+coderxiaofang@users.noreply.github.com>
2024-04-24 16:47:11 +0800

    Update method.java

--- IdeaProjects/objectProject/src/com/myblog/javaproject/method.java
+++ IdeaProjects/objectProject/src/com/myblog/javaproject/method.java
@@ -8,7 +8,6 @@
         ctrl+alt+M提取方法，可定义方法名
         home快速定位到行首，end快速定位到行尾
         Alt加鼠标左键竖着选中多个
-        
         Alt+insert或者右键点击Ptg to Javabean，快速生成JavaBean方法：
         ctrl+f 全文查找
         ctrl+alt+T 添加while循环
